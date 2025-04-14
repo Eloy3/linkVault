0427aa1884bdd6c01b920175890d7d88ec386660
karthala <eloy@Eloy.>
2025-04-14 22:19:51 +0200

    feat: first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func pong(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "pong \n")
+}
+func main() {
+	http.HandleFunc("/ping", pong)
+	http.ListenAndServe(":8080", nil)
+}
